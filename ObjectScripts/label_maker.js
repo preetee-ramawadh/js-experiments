6e55c53ed19f4aed4b6dc1d18fa0ffbcc9c261d4
preetee-awadh <preetee-awadh@pluralsight.com>
2024-05-13 10:38:20 +0530

    Understanding Objects in JS

---
+++ ObjectScripts/label_maker.js
@@ -0,0 +1,19 @@
+"use strict";
+
+let contact = {
+    name: "Preetee Ram Awadh",
+    address: "Shreeji",
+    city: "Thane",
+    state: "Maharashtra",
+    zip: "400607"
+}
+
+function printContact(contact){
+    console.log(contact.name);
+    console.log(contact.address);
+    console.log(contact.city);
+    console.log(contact.state);
+    console.log(contact.zip);
+}
+
+printContact(contact);
